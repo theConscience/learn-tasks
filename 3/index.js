911aacb06b5f0fb2dc60f0a36db06f15a9172145
VintoN <vinton.mail@gmail.com>
2020-02-10 23:38:15 +0300

    make 3rd work on complex queries

--- 3/index.js
+++ 3/index.js
@@ -1,36 +1,57 @@
 function cashBox(cashBillsQuery) {
+    console.log('cashBox() called with:', cashBillsQuery)
     const price = 25
     let cashSum = 0
     let availableBills = []
+    let unusedBills = []
     let hasShortChange = false
-    
+
     for (let i = 0; i < cashBillsQuery.length; i++) {
+        console.log('------- iteration', i, '-------')
+        console.log('cashSum:', cashSum)
         let currentBill = cashBillsQuery[i]
+        console.log('currentBill:', currentBill)
         if (currentBill === price) hasShortChange = true
         else if (currentBill > price) {
-            let changeAmount = currentBill - price
-            const enoughChange = changeAmount <= cashSum
+            let remainingAmount = currentBill - price
+            console.log('remainingAmount:', remainingAmount)
+            const enoughChange = remainingAmount <= cashSum
+            console.log('enoughChange:', enoughChange)
             if (!enoughChange) {  // seller has not enough change
                 hasShortChange = false
                 break
             } else {  // checking if bills suitable for make proper change
+                console.log('availableBills:', availableBills)
                 for (let j = 0; j < availableBills.length; j++) {
-                    changeAmount -= availableBills[j]
-                    if (changeAmount === 0) {
-                        availableBills = availableBills.slice(0, j)
+                    if (remainingAmount - availableBills[j] > 0) {
+                        remainingAmount -= availableBills[j]
+                    } else if (remainingAmount - availableBills[j] === 0) {
+                        remainingAmount = 0
+                        console.log('availableBills before:', availableBills)
+                        // availableBills = availableBills.slice(j + 1)
+                        availableBills.splice(0, j + 1)
+                        console.log('availableBills after :', availableBills)
                         break
+                    } else if (remainingAmount - availableBills[j] < 0) {
+                        unusedBills.push(availableBills[j])
+                        console.log('unusedBills:', unusedBills)
                     }
                 }
-                if (changeAmount === 0) hasShortChange = true
+                console.log('remainingAmount:', remainingAmount)
+                if (remainingAmount === 0) hasShortChange = true
                 else {
                     hasShortChange = false
                     break
                 }
             }
         }  // else break  // currentBill < price, cheater!
+        console.log('hasShortChange:', hasShortChange)
         cashSum += price
         availableBills.push(currentBill)
+        availableBills = [...unusedBills, ...availableBills]
+        unusedBills = []
     }
+    console.log('Has short change?', hasShortChange)
     return hasShortChange
 }
 
@@ -46,11 +67,24 @@ check(cashBox([25, 100]), false)
 check(cashBox([25, 25, 100]), false)
 check(cashBox([25, 25, 25, 100]), true)
 check(cashBox([25, 25, 50, 50, 100]), false)
+check(cashBox([25, 25, 50, 50, 100]), false)
+check(cashBox([25, 25, 50, 50, 100]), false)
+check(cashBox([25, 50, 25, 100]), true)
+check(cashBox([25, 50, 25, 100, 125]), true)
+check(cashBox([25, 50, 25, 100, 125, 150]), true)
+check(cashBox([25, 25, 50, 25, 25, 100]), true)
+check(cashBox([25, 25, 50, 25, 25, 125]), true)
+check(cashBox([25, 25, 50, 25, 25, 25, 150]), true)
+check(cashBox([25, 25, 50, 25, 25, 50, 50]), true)
+check(cashBox([25, 25, 50, 25, 25, 25, 25, 25, 25, 25, 25, 25, 50, 50]), true)
+check(cashBox([25, 25, 50, 25, 25, 25, 25, 25, 25, 25, 25, 25, 50, 50, 50, 100, 100, 500]), false)
+check(cashBox([25, 25, 50, 25, 25, 25, 25, 25, 25, 25, 25, 25, 50, 50, 50, 25, 100, 100, 100, 500]), true)
 
 
 // testing function
 function check(expression, value) {
     const checkResult = expression === value
-    checkResult ? console.log('Successfull check\n======') : console.warn('Check failed!\n======')
+    checkResult ? console.log('=================\nSuccessfull check\n=================') : console.warn('===========\nCheck failed!\n=============')
+    console.log('\n')
     return checkResult
 }
