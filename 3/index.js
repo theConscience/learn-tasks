28c1aea91a5cec6ccbd2f336b263939c34abbeeb
VintoN <vinton.mail@gmail.com>
2020-02-10 21:55:26 +0300

    updates in 3rd task

--- 3/index.js
+++ 3/index.js
@@ -1,25 +1,19 @@
 function cashBox(cashBillsQuery) {
-    console.log('cashBox() called with:', cashBillsQuery)
     const price = 25
     let cashSum = 0
     let availableBills = []
     let hasShortChange = false
     
     for (let i = 0; i < cashBillsQuery.length; i++) {
-        console.log('cashSum:', cashSum)
         let currentBill = cashBillsQuery[i]
-        console.log('currentBill:', currentBill)
         if (currentBill === price) hasShortChange = true
         else if (currentBill > price) {
             let changeAmount = currentBill - price
-            console.log('changeAmount:', changeAmount)
             const enoughChange = changeAmount <= cashSum
-            console.log('enoughChange:', enoughChange)
             if (!enoughChange) {  // seller has not enough change
                 hasShortChange = false
                 break
             } else {  // checking if bills suitable for make proper change
-                console.log('availableBills:', availableBills)
                 for (let j = 0; j < availableBills.length; j++) {
                     changeAmount -= availableBills[j]
                     if (changeAmount === 0) {
@@ -27,19 +21,16 @@ function cashBox(cashBillsQuery) {
                         break
                     }
                 }
-                console.log('changeAmount:', changeAmount)
                 if (changeAmount === 0) hasShortChange = true
                 else {
                     hasShortChange = false
                     break
                 }
             }
-        } else break  // currentBill < price, cheater!
-        console.log('hasShortChange:', hasShortChange)
+        }  // else break  // currentBill < price, cheater!
         cashSum += price
         availableBills.push(currentBill)
     }
-    console.log('Has short change?', hasShortChange)
     return hasShortChange
 }
 
